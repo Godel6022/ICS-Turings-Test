01f9fe2a47306e621c3105af4861e3850e7f0d93
JNKM <54767156+Godel6022@users.noreply.github.com>
2021-08-31 12:22:22 +0900

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,30 @@
+# Write Your Name Here
+# Name :
+
+
+# Problem 1 : N-Bonacci Sequence
+
+def problem_1(N, index) :
+    # Your Code Goes Here
+    return sequence
+
+
+# Problem 2 : DNA Base Paring
+
+def problem_2(strand1, strand2) :
+    # Your Code Goes Here
+    return false_pairs
+
+
+# Problem 3 : Palindrome
+
+def problem_3(number) :
+    # Your Code Goes Here
+    return maximum
+
+
+# Problem 4 : Caesar Code
+
+def problem_4(sample_word, sample_code, word) :
+    # Your Code Goes Here
+    return code
